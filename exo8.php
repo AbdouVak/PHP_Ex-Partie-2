3b76db19bf2b514a01952381836f662ad565d937
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-17 11:41:58 +0200

    exo8: img src afficher des images a partir des url

---
+++ exo8.php
@@ -0,0 +1,20 @@
+<h1>Exercice 8</h1>
+
+<p>Soit l’URL suivante : http://my.mobirise.com/data/userpic/764.jpg
+Créer une fonction personnalisée permettant d’afficher l’image N fois à l’écran.</p>
+
+<h2>Résultat</h2>
+
+
+<?PHP
+$url = "http://my.mobirise.com/data/userpic/764.jpg";
+
+function repeterImage($url,$nb_repetition){
+    while($nb_repetition != 0 ){
+        echo "<img src='$url'>";
+        $nb_repetition --;
+    }
+};
+
+repeterImage($url,5)
+?>
